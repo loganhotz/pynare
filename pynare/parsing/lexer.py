092579b31de7209cff77c01d882ca98d879c7196
hotzsauce <githotz@gmail.com>
2022-03-05 21:02:16 -0600

    augmented BaseLexer
    - an 'exp' parameter is now accepted upon initialization, which
        determines the Token that corresponds to exponentiation.
    - the BaseLexer was changed to be entirely stand-alone. base cases
        are taken into account for comments, valid characters for beginnings
        of variables, etc.
    - the workhorse 'next_token' method is corrected with respect to
        the tokenization of operators that share the first character
    
    the ExprLexer class was also removed

--- pynare/parsing/lexer.py
+++ pynare/parsing/lexer.py
@@ -18,17 +18,25 @@ class BaseLexer(object):
     language-agnostic lexer
     """
 
+    reserved_kw = base.reserved_funcs
+
     def __init__(
         self,
-        text: str
+        text: str,
+        exp: str = 'standard'
     ):
         self.text = text
+
+        if exp not in ('standard', 'python'):
+            raise ValueError(f"'{exp}'. 'exp' must be one of 'standard' or 'python'")
+        self.exp = exp
 
         self.line_pos = self.pos = 0
         self.current_line = 1 # I don't know any text editors that 0-index rows
         self.current_char = self.text[self.pos]
 
 
+    # basic lexer functionality
     def tokenize(self):
         self.token_pos = 0
         self.token_stream = list()
@@ -39,10 +47,8 @@ class BaseLexer(object):
 
         self.token_stream.append(Token(base.EOF, None, self.current_line, self.line_pos))
 
-
     def reset_lexer(self):
         self.token_pos = 0
-
 
     def error(self, symbol):
         if symbol:
@@ -52,6 +58,7 @@ class BaseLexer(object):
             )
 
 
+    # moving between and viewing tokens
     def advance(self, n=1):
         self.pos += n
         self.line_pos += n
@@ -65,7 +72,6 @@ class BaseLexer(object):
         except IndexError:
             self.current_char = None
 
-
     def peek(self, n=1):
         peek_pos = self.pos + n
         try:
@@ -74,11 +80,60 @@ class BaseLexer(object):
             return None
 
 
+    # methods intended for use when parsing
+    def get_next_token(self):
+        """
+        Returns the token at the current token position (it's the next token from
+        the Parser's perspective, however, hence the name), and iterates the token
+        position by one
+        """
+
+        # if the token stream is ran through multiple times for some reason, we 
+        #    need to reset the token position
+        if self.token_pos >= len(self.token_stream):
+            self.token_pos += 1 # self.token_pos = 0 ????
+
+        token = self.token_stream[self.token_pos]
+        self.token_pos += 1
+        return token
+
+    def see_future_token(self, k):
+        future_pos = (self.token_pos - 1) + k
+        if future_pos > len(self.token_stream) - 1:
+            raise SyntaxError('Trying to look too far into the future.')
+        else:
+            return self.token_stream[future_pos]
+
+    def see_next_token(self):
+        """
+        Returns the token at the current token position (it's the next token from
+        the Parser's perspective, however, hence the name), but unlike
+        self.get_next_token, the position is not incremented
+        """
+        return self.see_future_token(k=0)
+
+
+    # can reasonably expect this to be shared across all languages
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
 
+    # BaseLexer does not recognize any comments
+    def is_singleline_comment(self):
+        return False
+
+    def is_multiline_comment(self):
+        return False
+
+    def skip_singleline_comment(self):
+        pass
+
+    def skip_multiline_comment(self):
+        pass
+
+
+    # tokenizing specific object types: variable IDs, numbers, strings
     def _id(self):
         """
         recognizing characters or sequences of characters that may or may not be
@@ -96,7 +151,6 @@ class BaseLexer(object):
         token.assign_loc(self.current_line, self.line_pos)
         return token
 
-
     def _number(self):
         """
         recognizing numbers. We treat everything as a float, although Dynare (and
@@ -112,7 +166,6 @@ class BaseLexer(object):
         num = float(result)
         return Token(base.NUMBER, num, self.current_line, self.line_pos)
 
-
     def _string(self, quote):
         """
         recognizing literal strings, which can be enclosed by single or double
@@ -127,6 +180,46 @@ class BaseLexer(object):
         return Token(base.STRING, result, self.current_line, self.line_pos)
 
 
+    # one of my main issues with the current implementation are the next two methods
+    def is_exponent(self):
+        if (self.exp == 'standard') & (self.current_char == '^'):
+            return True
+        if (self.exp == 'python') & (self.current_char == '*') & (self.peek() == '*'):
+            return True
+        return False
+
+    def _exponent(self):
+        # is_exponent makes decision based on matlab's exponentiation symbol,
+        #    but we use python's exponentiation symbol for the token
+        if self.exp == 'python':
+            self.advance(2)
+        else:
+            self.advance()
+        return Token(base.POWER, '**', self.current_line, self.line_pos)
+
+
+    # for beginning & inner chars of IDs and numbers, use python paradigm because
+    #   its more forgiving than matlab. at odds with the 'exponent' methods, maybe?
+    def valid_varchar(self, c):
+        try:
+            return c.isalnum() or c == '_'
+        except AttributeError:
+            return False
+
+    def valid_begchar(self, c):
+        try:
+            return c.isalpha() or c == '_'
+        except AttributeError:
+            return False
+
+    def valid_numchar(self, c):
+        try:
+            return c.isdigit() or c == '.'
+        except AttributeError:
+            return False
+
+
+    # workhorse method
     def next_token(self):
 
         while self.current_char is not None:
@@ -152,7 +245,6 @@ class BaseLexer(object):
             if self.valid_numchar(self.current_char):
                 return self._number()
 
-
             #
             # Handling strings - not entirely sure what to do here. Matlab's 
             #    transpose character: " ' " can also be used make a character
@@ -164,7 +256,6 @@ class BaseLexer(object):
 
             if self.current_char == '\"':
                 return self._string(quote='\"')
-
 
             #
             # Grammar and Logical Symbols
@@ -213,21 +304,21 @@ class BaseLexer(object):
                 self.advance()
                 return Token(base.PERCENT, '%', self.current_line, self.line_pos)
 
-            if self.current_char == '<':
-                self.advance()
-                return Token(base.LT, '<', self.current_line, self.line_pos)
-
             if self.current_char == '<' and self.peek() == '=':
                 self.advance(2)
                 return Token(base.LTOE, '<=', self.current_line, self.line_pos)
 
-            if self.current_char == '>':
+            if self.current_char == '<':
                 self.advance()
-                return Token(base.GT, '>', self.current_line, self.line_pos)
+                return Token(base.LT, '<', self.current_line, self.line_pos)
 
             if self.current_char == '>' and self.peek() == '=':
                 self.advance(2)
                 return Token(base.GTOE, '>=', self.current_line, self.line_pos)
+
+            if self.current_char == '>':
+                self.advance()
+                return Token(base.GT, '>', self.current_line, self.line_pos)
 
             if self.current_char == '=' and self.peek() == '=':
                 self.advance(2)
@@ -237,10 +328,12 @@ class BaseLexer(object):
                 self.advance()
                 return Token(base.BACKSLASH, '\\', self.current_line, self.line_pos)
 
-
             # 
             # Common mathematical symbols
             # 
+            if self.is_exponent():
+                return self._exponent()
+
             if self.current_char == '+':
                 self.advance()
                 return Token(base.PLUS, '+', self.current_line, self.line_pos)
@@ -256,9 +349,6 @@ class BaseLexer(object):
             if self.current_char == '/':
                 self.advance()
                 return Token(base.DIV, '/', self.current_line, self.line_pos)
-
-            if self.is_exponent():
-                return self._exponent()
 
             if self.current_char == '(':
                 self.advance()
@@ -293,40 +383,6 @@ class BaseLexer(object):
         return Token(base.EOF, None, self.current_line, self.line_pos)
 
 
-    def get_next_token(self):
-        """
-        Returns the token at the current token position (it's the next token from
-        the Parser's perspective, however, hence the name), and iterates the token
-        position by one
-        """
-
-        # if the token stream is ran through multiple times for some reason, we 
-        #    need to reset the token position
-        if self.token_pos >= len(self.token_stream):
-            self.token_pos += 1 # self.token_pos = 0 ????
-
-        token = self.token_stream[self.token_pos]
-        self.token_pos += 1
-        return token
-
-
-    def see_future_token(self, k):
-        future_pos = (self.token_pos - 1) + k
-        if future_pos > len(self.token_stream) - 1:
-            raise SyntaxError('Trying to look too far into the future.')
-        else:
-            return self.token_stream[future_pos]
-
-
-    def see_next_token(self):
-        """
-        Returns the token at the current token position (it's the next token from
-        the Parser's perspective, however, hence the name), but unlike
-        self.get_next_token, the position is not incremented
-        """
-        return self.see_future_token(k=0)
-
-
 
 class DynareLexer(BaseLexer):
 
@@ -337,8 +393,9 @@ class DynareLexer(BaseLexer):
         text: str
     ):
 
-        super().__init__(text)
+        super().__init__(text, exp='standard')
 
+    # variable naming paradigms
     def valid_varchar(self, c):
         # dynare variable names can have numbers, letters, or underscores
         try:
@@ -353,13 +410,7 @@ class DynareLexer(BaseLexer):
         except AttributeError:
             return False
 
-    def valid_numchar(self, c):
-        # numbers can either begin with a decimal or number 
-        try:
-            return c.isdigit() or c == '.'
-        except AttributeError:
-            return False
-
+    # comments
     def is_singleline_comment(self):
         # single line comments are signified by '%' or '//'
         if (
@@ -369,13 +420,11 @@ class DynareLexer(BaseLexer):
             return True
         return False
 
-
     def skip_singleline_comment(self):
         """ single line comments are signified by '%' or '//' """
         while self.current_char != '\n':
             self.advance()
         self.skip_whitespace() # just in case
-
 
     def is_multiline_comment(self):
         if self.current_char == '/' and self.peek() == '*':
@@ -391,254 +440,3 @@ class DynareLexer(BaseLexer):
         # skipping the closing '*/'
         self.advance(n=2)
         self.skip_whitespace() # just in case
-
-    def is_exponent(self):
-        if self.current_char == '^':
-            return True
-        return False
-
-    def _exponent(self):
-        # is_exponent makes decision based on matlab's exponentiation symbol,
-        #    but we use python's exponentiation symbol for the token
-        self.advance()
-        return Token(base.POWER, '**', self.current_line, self.line_pos)
-
-
-
-class ExprLexer(object):
-    """
-    lexer that only handles simple mathematical expressions. recognizes numbers,
-    variable names, the standard set of functions, and comparative operators.
-
-    - the symbols '**' and '^' are both treated as exponents
-    - variable names follow python's pattern; ie they start with underscores or
-        letters
-    """
-
-    def __init__(
-        self,
-        text: str
-    ):
-        self.text = text
-
-        self.line_pos = self.pos = 0
-        self.current_line = 1 # I don't know any text editors that 0-index rows
-        self.current_char = self.text[self.pos]
-
-
-    def tokenize(self):
-        self.token_pos = 0
-        self.token_stream = list()
-
-        while self.current_char is not None:
-            t = self.next_token()
-            self.token_stream.append(t)
-
-        self.token_stream.append(Token(base.EOF, None, self.current_line, self.line_pos))
-
-
-    def reset_lexer(self):
-        self.token_pos = 0
-
-
-    def error(self, symbol):
-        if symbol:
-            raise SyntaxError(
-                f'unrecognized symbol: {repr(symbol)} in '
-                f'line {self.current_line}, column {self.line_pos}'
-            )
-
-
-    def advance(self, n=1):
-        self.pos += n
-        self.line_pos += n
-        try:
-            if self.current_char == '\n':
-                # moving line idx down to the next line, and resetting the line position 
-                self.current_line += 1
-                self.line_pos = 0
-            self.current_char = self.text[self.pos]
-
-        except IndexError:
-            self.current_char = None
-
-
-    def peek(self, n=1):
-        peek_pos = self.pos + n
-        try:
-            return self.text[peek_pos]
-        except IndexError:
-            return None
-
-
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
-
-    def valid_varchar(self, c):
-        if c.isalpha() or c == '_':
-            return True
-        else:
-            return False
-
-
-    def valid_numchar(self, c):
-        if c.isdigit() or c == '.':
-            return True
-        else:
-            return False
-
-
-    def _id(self):
-        """
-        recognizing characters or sequences of characters that may or may not be
-        recognized keywords. If they are not, we tag them as a Token of type ID,
-        and assume they're a variable
-        """
-        result = ''
-        while self.valid_varchar(self.current_char):
-            result += self.current_char
-            self.advance()
-        try:
-            token = self.reserved_kw[result]
-        except KeyError:
-            token = base.reserved_funcs.get(result, Token(base.ID, result))
-        token.assign_loc(self.current_line, self.line_pos)
-        return token
-
-
-    def _number(self):
-        """
-        recognizing numbers. We treat everything as a float, although Dynare (and
-        Matlab underneath it) treats ints and floats as distinct types
-        """
-        result = ''
-        while (
-            self.current_char is not None
-            and self.valid_numchar(self.current_char)
-        ):
-            result += self.current_char
-            self.advance()
-        num = float(result)
-        return Token(base.NUMBER, num, self.current_line, self.line_pos)
-
-
-    def next_token(self):
-
-        while self.current_char is not None:
-
-            try:
-                while self.current_char.isspace():
-                    self.skip_whitespace()
-            except AttributeError:
-                if self.current_char is None:
-                    return Token(base.EOF, None, self.current_line, self.line_pos)
-                else:
-                    self.error(self.current_char)
-
-            if self.valid_varchar(self.current_char):
-                return self._id()
-
-            if self.valid_numchar(self.current_char):
-                return self._number()
-
-            #
-            # operators
-            #
-            if self.current_char == '<':
-                self.advance()
-                return Token(base.LT, '<', self.current_line, self.line_pos)
-
-            if self.current_char == '<' and self.peek() == '=':
-                self.advance(2)
-                return Token(base.LTOE, '<=', self.current_line, self.line_pos)
-
-            if self.current_char == '>':
-                self.advance()
-                return Token(base.GT, '>', self.current_line, self.line_pos)
-
-            if self.current_char == '>' and self.peek() == '=':
-                self.advance(2)
-                return Token(base.GTOE, '>=', self.current_line, self.line_pos)
-
-            if self.current_char == '=' and self.peek() == '=':
-                self.advance(2)
-                return Token(base.EQUALITY, '==', self.current_line, self.line_pos)
-
-            # 
-            # common mathematical symbols
-            # 
-            if self.current_char == '*' and self.peak() == '*':
-                self.advance(2)
-                return Token(base.POWER, '**', self.current_line, self.line_pos)
-
-            if self.current_char == '^':
-                self.advance()
-                return Token(base.POWER, '**', self.current_line, self.line_pos)
-
-            if self.current_char == '+':
-                self.advance()
-                return Token(base.PLUS, '+', self.current_line, self.line_pos)
-
-            if self.current_char == '-':
-                self.advance()
-                return Token(base.MINUS, '-', self.current_line, self.line_pos)
-
-            if self.current_char == '*':
-                self.advance()
-                return Token(base.MUL, '*', self.current_line, self.line_pos)
-
-            if self.current_char == '/':
-                self.advance()
-                return Token(base.DIV, '/', self.current_line, self.line_pos)
-
-            if self.current_char == '(':
-                self.advance()
-                return Token(base.LPARE, '(', self.current_line, self.line_pos)
-
-            if self.current_char == ')':
-                self.advance()
-                return Token(base.RPARE, ')', self.current_line, self.line_pos)
-
-            if self.current_char == '=':
-                self.advance()
-                return Token(base.EQUALS, '=', self.current_line, self.line_pos)
-
-            self.error(self.current_char)
-
-        return Token(base.EOF, None, self.current_line, self.line_pos)
-
-
-    def get_next_token(self):
-        """
-        Returns the token at the current token position (it's the next token from
-        the Parser's perspective, however, hence the name), and iterates the token
-        position by one
-        """
-
-        # if the token stream is ran through multiple times for some reason, we 
-        #    need to reset the token position
-        if self.token_pos >= len(self.token_stream):
-            self.token_pos += 1 # self.token_pos = 0 ????
-
-        token = self.token_stream[self.token_pos]
-        self.token_pos += 1
-        return token
-
-
-    def see_future_token(self, k):
-        future_pos = (self.token_pos - 1) + k
-        if future_pos > len(self.token_stream) - 1:
-            raise SyntaxError('Trying to look too far into the future.')
-        else:
-            return self.token_stream[future_pos]
-
-
-    def see_next_token(self):
-        """
-        Returns the token at the current token position (it's the next token from
-        the Parser's perspective, however, hence the name), but unlike
-        self.get_next_token, the position is not incremented
-        """
-        return self.see_future_token(k=0)
